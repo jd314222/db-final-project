e4ac7fbefb544c4ba795b9ea4898c59ffedaae09
jd314222 <jd314222@ohio.edu>
2025-12-02 22:55:04 -0500

    Uncommented data downloads

--- download_dataset.py
+++ download_dataset.py
@@ -7,8 +7,8 @@ import os
 import shutil
 
 # Download latest version
-# path = kagglehub.dataset_download("akashunikaggle/steam-game-reviews-of-743-games")
-# path = kagglehub.dataset_download("baraazaid/pc-video-game-requirements")
+path = kagglehub.dataset_download("akashunikaggle/steam-game-reviews-of-743-games")
+path = kagglehub.dataset_download("baraazaid/pc-video-game-requirements")
 path = kagglehub.dataset_download("fronkongames/steam-games-dataset")
 
 print("Path to dataset files:", path)
